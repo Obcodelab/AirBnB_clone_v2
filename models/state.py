2b0611ec5c32c49e801605b1acdf40dbca8d724f
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-21 07:24:12 -0700

    initialise state class attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models import storage
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all,delete")
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id
+        """
+
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                return city
