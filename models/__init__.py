73986845726f9880ada388a93e066c9c75b978a1
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-21 13:51:13 -0700

    add database storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates an object of class FileStorage
+    and DBStorage """
+import os
 
 
-storage = FileStorage()
-storage.reload()
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+if type_storage == 'db':
+    from models.engine.db_storage import DBStorage
+
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+
+    storage = FileStorage()
+    storage.reload()
