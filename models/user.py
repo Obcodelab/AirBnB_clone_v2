d561973891afbc3d774ba643b665031ed194a90e
obcodelab <gideonayomide75@gmail.com>
2023-06-21 23:00:08 +0100

    updated User

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """Defines the User class."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """Represents a user for a MySQL database.
@@ -15,10 +16,10 @@ class User(BaseModel, Base):
         first_name (sqlalchemy String): The user's first name.
         last_name (sqlalchemy String): The user's last name.
         places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
