e3958cae051ce2868da6646255c7874ec874008a
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-23 11:45:55 -0700

    add relationship import

--- models/place.py
+++ models/place.py
@@ -2,6 +2,8 @@
 """Defines the Place class."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, backref
+
 
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
@@ -25,7 +27,7 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024),nullable=True)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
