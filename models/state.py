97d3a88b89ddbc569b58991f26793c84d510f7c5
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-24 09:37:55 -0700

    update state class

--- models/state.py
+++ models/state.py
@@ -3,26 +3,30 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship, backref
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
 
-    __tablename__ = "states"
+    __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all,delete")
-
-    @property
-    def cities(self):
-        """Getter attribute cities that returns the list of City
+    
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db': 
+        cities = relationship('City', backref='state', cascade='all,delete')
+    else:
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City
             instances with state_id equals to the current State.id
-        """
+            """
 
-        from models import storage
+            from models import storage
+            from models.city import City
 
-        a_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                a_list.append(city)
-        return a_list
+            a_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    a_list.append(city)
+            return a_list
