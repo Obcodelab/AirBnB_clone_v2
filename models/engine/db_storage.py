4fde1c1d9ef6b2ae5bd523b7256b0f1e94690aa5
obcodelab <gideonayomide75@gmaail.com>
2023-07-23 17:09:25 -0700

    updated with close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,3 +79,7 @@ class DBStorage:
 
         self.__session = scoped_session(sessionmaker(
             bind=self.__engine, expire_on_commit=False))
+
+    def close(self):
+        """Remove private session attribute"""
+        self.__session.close()
